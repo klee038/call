da4b45346d15afec30bb7c2a58253d4c4c6f4c2a
klee038 <shintiro.m@gmail.com>
2026-06-24 22:50:28 +0900

    Add files via upload

---
+++ toss.js
@@ -0,0 +1,34 @@
+﻿// =========================================
+// トス専用ロジック (toss.js)
+// =========================================
+
+let tossWinner = null;    
+let winnerChoice = null;  
+let loserChoice = null;   
+
+function getTossName(t, p1, p2) {
+  let pStr = flowIsDouble ? `${p1} / ${p2}` : p1;
+  if (t.trim().length > 0) {
+    return `${t}\n<span style="font-size:11px;">(${pStr})</span>`;
+  } else {
+    return flowIsDouble ? `${p1}\n${p2}` : p1;
+  }
+}
+
+function setTossWinner(val) {
+  tossWinner = val; 
+  winnerChoice = null; 
+  loserChoice = null;
+  renderFlow();
+}
+
+function setWinnerChoice(val) {
+  winnerChoice = val; 
+  loserChoice = null;
+  renderFlow();
+}
+
+function setLoserChoice(val) {
+  loserChoice = val;
+  renderFlow();
+}
